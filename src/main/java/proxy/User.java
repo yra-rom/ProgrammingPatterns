5c931d9136d1618b76cb7ae6bf472558b9d4a689
Yurii Piets <yurii.piets@gmail.com>
2017-06-15 22:26:02 +0200

    proxy pattern and tests for it

---
+++ src/main/java/proxy/User.java
@@ -0,0 +1,27 @@
+package proxy;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User implements Cloneable {
+    private String id;
+    private String name;
+    private String surname;
+
+    public User(User user){
+        this.id = user.id;
+        this.name = user.name;
+        this.surname = user.surname;
+    }
+
+    @Override
+    public User clone() {
+        return new User(this);
+    }
+}
